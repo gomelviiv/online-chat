c1250b8fefedb1b87c76e9b5a019e5db1f60e49b
gomeviiv <gomelviiv@gmail.com>
2020-04-14 23:21:27 +0300

    add 1 component and delete 2 comp

--- front/src/components/sign/sign.jsx
+++ front/src/components/sign/sign.jsx
@@ -1,30 +1,77 @@
 import React, { useState } from 'react'
+import './sign.css'
 
-function Sign(){
-    const [count, setState] = useState(0);
-    
+import { signFetch } from '../../smartComponents/fetchContainer.jsx'
+import  { Redirect } from 'react-router'
+
+export default function Sign(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [againPassword, setAgainPassword] = useState('');
+    const [auth, setAuth] = useState(''); 
+    const [check, setCheck] = useState(true);
+
+    const sendMessage = () => {
+        if(check){
+            signFetch({email, password}, 'signIn').then(data => {
+                if(data){
+                    setAuth(data.auth)
+                    localStorage.setItem('token', data.token)
+                }else{
+                    alert('Вы ввели некорректные данные, попробуйте еще раз')
+                }
+            })
+        } else {
+            if(password == againPassword){ 
+                signFetch({email, password}, 'signUp').then(data => {
+                    if(data){
+                        alert('Всё четко')
+                        setCheck(true)
+                    } else {
+                        alert('Пользователь с таким Email уже зарегестрирован в сети')
+                    }
+                })
+            } else {
+                alert('Пароли не совпадают')
+            }
+        }
+    }
+
+    if (auth) {
+        return <Redirect to='/main'/>
+    }
+
     return (
-        <div className="sign-up">
+        <div className="sign">
             <form>
-                <div class="form-group">
+                <div className="form-group">
                     <label for="exampleInputEmail1">Email address</label>
                     <input 
                         type="email" 
-                        class="form-control" 
-                        onChange={(e)=>this.changeInput('email', e.target.value)} 
+                        className="form-control" 
+                        onChange={(e)=>setEmail(e.target.value)} 
                         id="exampleInputEmail1" 
                         aria-describedby="emailHelp"
                     />
                 </div>
-                <div class="form-group">
+                <div className="form-group">
                     <label for="exampleInputPassword1">Password</label>
                     <input 
-                        class="form-control" 
-                        onChange={(e)=>this.changeInput('password', e.target.value)} 
+                        className="form-control" 
+                        onChange={(e)=>setPassword(e.target.value)} 
                         id="exampleInputPassword1"
                     />
                 </div>
-                <button type="submit" onClick={()=>this.sendMessage()} class="btn btn-primary">Registration</button>
+                {   !check ? <div className="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input 
+                            className="form-control" 
+                            onChange={(e)=>setAgainPassword(e.target.value)} 
+                            id="exampleInputPassword2"
+                        />
+                    </div> : ''} 
+                <button type="submit" onClick={()=>sendMessage(setCheck(true))} className="btn btn-primary">Sign in</button>
+                <button type="submit" onClick={()=>sendMessage(setCheck(false))} className="btn btn-primary">Sign up</button>
             </form>
         </div>
     )
